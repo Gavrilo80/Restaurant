353059becb7cac55da88adbc140148f935796adb
Gavra <gavriloilic80@gmail.com>
2024-01-12 11:55:47 +0100

    made a order request for customer

--- src/customer/orders/index.ts
+++ src/customer/orders/index.ts
@@ -2,6 +2,8 @@ import { Router, request, response } from "express";
 import Customer from "../../models/Customer";
 import Orders from "../../models/Orders";
 import Reward from "../../models/Reward";
+import FoodMenu from "../../models/FoodMenu";
+import FoodmenuOrders from "../../models/FoodmenuOrders";
 
 const router = Router();
 
@@ -10,43 +12,56 @@ router.get("/", async (req: request, res: response) => {
     where: { CustomerId: req.authEntety.user_id },
     include: Reward
   })
-  res.json({ message: "Customer order with id!", orders: orders });
+  res.json({ message: "Customer order with id!", orders });
 });
-
 
 router.post("/", async (req: request, res: response) => {
-  const { food_menu, drink_menu, total_ammount } = req.body;
+  const orderItems = req.body;
 
-  const customer = await Customer.findOne({
-    where: { id: req.authEntety.user_id }
-  }) as any;
-
-  const orders = await customer.createOrder({ food_menu, drink_menu, total_ammount });
-
-  // Reward for the customer when entering the ammount 
-  if (orders.total_ammount >= 1000 && orders.total_ammount < 1999) {
-
-    orders.setReward(2)
-
-  } else if (orders.total_ammount >= 2000 && orders.total_ammount < 2999) {
-
-    orders.setReward(3)
-
-  } else if (orders.total_ammount >= 3000 && orders.total_ammount < 3999) {
-
-    orders.setReward(4)
-
-  } else if (orders.total_ammount >= 4000) {
-
-    orders.setReward(5)
-
-  }
-  else {
-    return res.send("Thanks for Ordering!")
+  // You extract the total amount from the quantity and price 
+  let totalOrderPrice = 0;
+  for (const orderItem of orderItems) {
+    const foodMenuItem = await FoodMenu.findOne({ where: { id: orderItem.id } }) as any
+    totalOrderPrice += (orderItem.quantity ?? 1) * foodMenuItem.price;
   }
 
-  res.json({ message: "Create Order For Customer", orders });
-});
+  // Create a order with the total amount and the customer ID
+  const order = await Orders.create({
+    total_ammount: totalOrderPrice,
+    customerId: req.authEntety.user_id
+  }) as any
+
+  // Reward for the customer depending on the amount
+  if (order.total_ammount >= 20 && order.total_ammount < 29) {
+
+    order.setReward(2)
+
+  } else if (order.total_ammount >= 30 && order.total_ammount < 39) {
+
+    order.setReward(3)
+
+  } else if (order.total_ammount >= 40 && order.total_ammount < 49) {
+
+    order.setReward(4)
+
+  } else if (order.total_ammount >= 50) {
+
+    order.setReward(5)
+
+  }
+
+  // Fill in the table model with all the necessary data
+  for (const orderItem of orderItems) {
+    await FoodmenuOrders.create({
+      foodMenuId: orderItem.id,
+      orderId: order.id,
+      quantity: orderItem.quantity
+    })
+  }
+
+  res.json({ message: "Order Created Successfully!", order })
+})
+
 
 router.put("/:id", async (req: request, res: response) => {
   const { food_menu, drink_menu, total_ammount } = req.body;
@@ -71,7 +86,6 @@ router.delete("/:id", async (req: request, res: response) => {
 
   res.json(orders);
 })
-
 
 
 
