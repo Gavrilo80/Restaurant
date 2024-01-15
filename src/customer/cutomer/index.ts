ad18f9706fb2e1eb2a843de7577295d0c2dfa4b0
Gavra <ilicgavrilo80@gmail.com>
2024-01-15 17:10:49 +0100

    Adding put method for updata Customer

--- src/customer/cutomer/index.ts
+++ src/customer/cutomer/index.ts
@@ -1,5 +1,6 @@
 import { Router, request, response } from "express";
 import Customer from "../../models/Customer";
+import bcrypt from "bcryptjs";
 
 const router = Router();
 
@@ -12,7 +13,32 @@ router.get("/", async (req: request, res: response) => {
 })
 
 router.put("/:id", async (req: request, res: response) => {
-  res.json("everything updated Successfully!")
+  const { email, phone, username, password } = req.body;
+
+  const customer = await Customer.findOne({ where: { id: req.authEntety.user_id } }) as any;
+  // console.log('req.params.id:', req.params.id); String
+  // console.log('customer.id:', customer.id);  Number
+
+  // Checks if it a logged in user
+  if (req.params.id != customer.id) {
+    return res.status(401).json({ error: 'Unauthorized!!!' });
+  }
+
+  // Authenticate the user first
+  if (!customer || !(await bcrypt.compare(password, customer.password))) {
+    return res.status(401).json({ error: 'Invalid credentials' });
+  }
+
+  // If authentication is successful, update the user's information
+  const updateCustomer = await Customer.update({
+    email,
+    phone,
+    username,
+    password: await bcrypt.hash(password, 10)
+  }, {
+    where: { id: req.params.id }
+  })
+  res.json({ message: "Everything Updated Successfully!", updateCustomer })
 })
 
 
